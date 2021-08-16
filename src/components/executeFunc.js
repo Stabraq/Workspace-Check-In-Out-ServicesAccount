7d79c172f66b498c2d8b2fdedba01c57f64d2441
mero2online <mero2online@gmail.com>
2021-08-16 14:05:32 +0200

    Change cost to flat rate after 6HR

--- src/components/executeFunc.js
+++ src/components/executeFunc.js
@@ -182,7 +182,7 @@ export const executeValuesAppendCheckOut = async (
           [
             `=IF(H${rowNumber}*24<1,1,IF(OR(AND(H${rowNumber}*24-INT(H${rowNumber}*24)<=0.1),AND(H${rowNumber}*24-INT(H${rowNumber}*24)>0.5,H${rowNumber}*24-INT(H${rowNumber}*24)<=0.59)),FLOOR(H${rowNumber},"00:30")*24,CEILING(H${rowNumber},"00:30")*24))`,
           ],
-          membership.includes('Not Member') ? [`=I${rowNumber}*10`] : [''],
+          membership.includes('Not Member') ? [`=IF(I${rowNumber}>=6,60,I${rowNumber}*10)`] : [''],
           [checkInOut],
         ],
       },
